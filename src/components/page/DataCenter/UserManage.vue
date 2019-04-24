f0b350a969a156b0324f15ef0e9fc30b808c6861
battleingame <you@example.com>
2019-04-25 00:22:17 +0800

    弹窗增加

--- src/components/page/DataCenter/UserManage.vue
+++ src/components/page/DataCenter/UserManage.vue
@@ -17,6 +17,64 @@
           ></el-option>
         </el-select>
         <el-button type="primary">查询</el-button>
+        <el-button type="success" @click="dialogFormVisible = true">新增</el-button>
+        <el-dialog title="添加人员" :visible.sync="dialogFormVisible">
+          <el-form :model="form">
+            <el-form-item label="人员信息：" :label-width="formLabelWidth"></el-form-item>
+            <div class="flex">
+              <el-form-item label="人员类型" :label-width="formLabelWidth">
+                <el-select v-model="formInline.region">
+                  <el-option label="现役人员" value="shanghai"></el-option>
+                  <el-option label="退役人员" value="beijing"></el-option>
+                  <el-option label="驻训人员" value="beijing"></el-option>
+                  <el-option label="职工" value="beijing"></el-option>
+                  <el-option label="家属" value="beijing"></el-option>
+                  <el-option label="临时人员" value="beijing"></el-option>
+                </el-select>
+              </el-form-item>
+
+              <el-form-item label="所属部门" :label-width="formLabelWidth">
+                <el-select v-model="formInline.region">
+                  <el-option label="战斗班" value="shanghai"></el-option>
+                </el-select>
+              </el-form-item>
+            </div>
+            <el-form-item label="基础信息：" :label-width="formLabelWidth"></el-form-item>
+            <div class="flex">
+              <el-form-item label="姓名：" :label-width="formLabelWidth">
+                <el-input v-model="formInline.user" :label-width="formLabelWidth" placeholder="请输入"></el-input>
+              </el-form-item>
+              <el-form-item label="性别" :label-width="formLabelWidth">
+                <el-radio v-model="radio" label="1">男</el-radio>
+                <el-radio v-model="radio" label="2">女</el-radio>
+              </el-form-item>
+            </div>
+            <div class="flex">
+              <el-form-item label="身份证号：" :label-width="formLabelWidth">
+                <el-input v-model="formInline.user" :label-width="formLabelWidth" placeholder="请输入"></el-input>
+              </el-form-item>
+              <el-form-item label="年龄：" :label-width="formLabelWidth">33</el-form-item>
+            </div>
+            <el-form-item label="联系电话：" :label-width="formLabelWidth">
+              <el-input v-model="formInline.user" style="width:200px" placeholder="请输入"></el-input>
+            </el-form-item>
+            <el-form-item label="内部联系人" :label-width="formLabelWidth">
+              <el-select v-model="formInline.region">
+                <el-option label="一团" value="shanghai"></el-option>
+                <el-option label="二团" value="beijing"></el-option>
+                <el-option label="三团" value="beijing"></el-option>
+              </el-select>
+              <el-select v-model="formInline.region" placeholder="请选择" style="margin-left:20px;">
+                <el-option label="张三" value="shanghai"></el-option>
+                <el-option label="李四" value="beijing" ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+          </div>
+        </el-dialog>
       </div>
       <div class="body">
         <el-table
@@ -24,10 +82,10 @@
           style="width: 100%"
           :default-sort="{prop: 'name', order: 'descending'}"
         >
-          <el-table-column prop="name" label="姓名" >{{}}</el-table-column>
-          <el-table-column prop="gender" label="性别" ></el-table-column>
-          <el-table-column prop="dept" label="部门" ></el-table-column>
-          <el-table-column prop="job" label="职位" ></el-table-column>
+          <el-table-column prop="name" label="姓名"></el-table-column>
+          <el-table-column prop="gender" label="性别"></el-table-column>
+          <el-table-column prop="dept" label="部门"></el-table-column>
+          <el-table-column prop="job" label="职位"></el-table-column>
           <el-table-column prop="tel" label="联系电话"></el-table-column>
           <el-table-column prop="type" label="人员类型"></el-table-column>
           <el-table-column label="操作">
@@ -46,6 +104,8 @@ export default {
   name: "documentManagement",
   data() {
     return {
+      radio: 1,
+      dialogFormVisible: false,
       value1: "",
       value2: "",
       input2: "",
@@ -73,7 +133,7 @@ export default {
           name: "李云龙",
           gender: "男",
           dept: "保卫科",
-          job:"保卫员",
+          job: "保卫员",
           tel: "1388585452",
           type: "现役"
         },
@@ -81,7 +141,7 @@ export default {
           name: "李云龙",
           gender: "男",
           dept: "保卫科",
-          job:"保卫员",
+          job: "保卫员",
           tel: "1388585452",
           type: "现役"
         },
@@ -89,7 +149,7 @@ export default {
           name: "李云龙",
           gender: "男",
           dept: "保卫科",
-          job:"保卫员",
+          job: "保卫员",
           tel: "1388585452",
           type: "现役"
         },
@@ -97,7 +157,7 @@ export default {
           name: "李云龙",
           gender: "男",
           dept: "保卫科",
-          job:"保卫员",
+          job: "保卫员",
           tel: "1388585452",
           type: "现役"
         }
@@ -136,7 +196,11 @@ export default {
         resource: "",
         desc: ""
       },
-      formLabelWidth: "120px"
+      formLabelWidth: "120px",
+      formInline: {
+        user: "",
+        region: ""
+      }
     };
   },
   methods: {
@@ -162,7 +226,7 @@ export default {
 .flex {
   display: flex;
 }
-.staffStatu{
-    margin: 0 20px;
+.staffStatu {
+  margin: 0 20px;
 }
 </style>
--- src/components/page/SysConfig/DicManage.vue
+++ src/components/page/SysConfig/DicManage.vue
@@ -17,18 +17,80 @@
             <el-button type="primary" @click="onSubmit">查询</el-button>
           </el-form-item>
           <el-form-item>
-            <el-button type="success" @click="dialogFormVisible = true">新增</el-button>
+            <el-button type="success" @click="outerVisible = true">新增</el-button>
           </el-form-item>
+          <el-dialog title="添加字典" :visible.sync="outerVisible">
+            <el-dialog width="30%" title="添加明细" :visible.sync="innerVisible" append-to-body>
+              <el-form :model="form">
+                <div class="flex">
+                  <el-form-item label="明细编号">
+                    <el-input v-model="formInline.user" :label-width="formLabelWidth" placeholder="请输入"></el-input>
+                  </el-form-item>
+                  <el-form-item label="显示值">
+                    <el-input v-model="formInline.user" :label-width="formLabelWidth" placeholder="请输入"></el-input>
+                  </el-form-item>
+                </div>
+                <el-form-item label="明细状态">
+                  <el-radio v-model="radio" label="1">启用</el-radio>
+                  <el-radio v-model="radio" label="2">禁用</el-radio>
+                </el-form-item>
+              </el-form>
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="innerVisible = false">取 消</el-button>
+                <el-button type="primary" @click="innerVisible = false">确 定</el-button>
+              </div>
+            </el-dialog>
+            <el-form :model="form">
+              <div class="flex">
+                <el-form-item label="字典名称">
+                  <el-input v-model="formInline.user" placeholder="请输入"></el-input>
+                </el-form-item>
+                <el-form-item label="备注">
+                  <el-input v-model="formInline.user" placeholder="请输入" type="textarea"></el-input>
+                </el-form-item>
+              </div>
+              <el-form-item label="字典状态">
+                <el-radio v-model="radio" label="1">启用</el-radio>
+                <el-radio v-model="radio" label="2">禁用</el-radio>
+              </el-form-item>
+              <div>
+                <el-form-item>
+                  <el-button type="primary" @click="innerVisible = true">添加明细</el-button>
+                </el-form-item>
+              </div>
+              <div>
+                <el-table
+                  :data="tableData"
+                  style="width: 100%"
+                  :default-sort="{prop: 'name', order: 'descending'}"
+                >
+                  <el-table-column prop="name" label="明细编码" sortable></el-table-column>
+                  <el-table-column prop="desc" label="显示值" sortable></el-table-column>
+                  <el-table-column prop="status" label="状态" sortable></el-table-column>
+                  <el-table-column prop="date" label="添加日期" sortable></el-table-column>
+                  <el-table-column label="操作" width="180">
+                    <template slot-scope="scope">
+                      <el-button size="mini" @click="innerVisible = true">编辑</el-button>
+                      <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">删除</el-button>
+                    </template>
+                  </el-table-column>
+                </el-table>
+              </div>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="outerVisible = false">取 消</el-button>
+              <el-button type="primary" @click="outerVisible = false">确 定</el-button>
+            </div>
+          </el-dialog>
         </el-form>
       </div>
       <div class="body">
-         <el-table
+        <el-table
           :data="tableData"
           style="width: 100%"
           :default-sort="{prop: 'name', order: 'descending'}"
         >
-          <el-table-column prop="name" label="字典名称" sortable width="180">
-          </el-table-column>
+          <el-table-column prop="name" label="字典名称" sortable width="180"></el-table-column>
           <el-table-column prop="desc" label="描述" sortable width="180"></el-table-column>
           <el-table-column prop="status" label="使用状态" sortable></el-table-column>
           <el-table-column prop="date" label="修改时间" sortable></el-table-column>
@@ -47,6 +109,7 @@ export default {
   name: "documentManagement",
   data() {
     return {
+      radio: 1,
       pickerOptions: {
         disabledDate(time) {
           return time.getTime() > Date.now();
@@ -76,10 +139,12 @@ export default {
           }
         ]
       },
+      outerVisible: false,
+      innerVisible: false,
       value1: "",
       value2: "",
       input2: "",
-      radioStatus:"",//新增-启用/禁用 单选
+      radioStatus: "", //新增-启用/禁用 单选
       options: [
         {
           value: "1",
@@ -106,25 +171,25 @@ export default {
       tableData: [
         {
           name: "警告级别",
-          desc:"警告分级",
+          desc: "警告分级",
           status: "启用",
           date: "2019-04-23"
         },
         {
           name: "警告级别",
-          desc:"警告分级",
+          desc: "警告分级",
           status: "启用",
           date: "2019-04-23"
         },
         {
           name: "警告级别",
-          desc:"警告分级",
+          desc: "警告分级",
           status: "启用",
           date: "2019-04-23"
         },
         {
           name: "警告级别",
-          desc:"警告分级",
+          desc: "警告分级",
           status: "启用",
           date: "2019-04-23"
         }
