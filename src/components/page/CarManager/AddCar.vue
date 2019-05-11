80a5617ce8b8236930633d4b1ef43efb83bea4e7
battleingame <you@example.com>
2019-05-11 16:57:40 +0800

    内部车辆 外部车辆tabs显示

--- src/components/page/CarManager/AddCar.vue
+++ src/components/page/CarManager/AddCar.vue
@@ -8,167 +8,192 @@
       </el-breadcrumb>
     </div>
     <div class="container">
-      <div class="header-container">
-        <div class="carImg">车辆照片</div>
-        <div class="carDet">
-          <div class="license">
-            奔驰2号
-            <span class=" used" >在用</span>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="内部车辆" name="first">
+          <div class="header-container">
+            <div class="carImg">车辆照片</div>
+            <div class="carDet">
+              <div class="license">
+                奔驰2号
+                <span class="used">在用</span>
+              </div>
+              <div>
+                车辆类型
+                <el-select class="input-width" v-model="inoutValue" placeholder="请选择">
+                  <el-option
+                    v-for="item in inoutOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>
+              </div>
+            </div>
           </div>
-          <div>
-            车辆类型
-            <el-select class="input-width" v-model="inoutValue" placeholder="请选择">
-              <el-option
-                v-for="item in inoutOptions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select>
+          <div class="footer">
+            <div class="one">
+              <p>车辆基本信息：</p>
+              <div class="itemDet">
+                车辆品牌：
+                <el-select class="input-width" v-model="brandValue" placeholder="请选择">
+                  <el-option
+                    v-for="item in brandOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>车辆型号：
+                <el-select class="input-width" v-model="modelValue" placeholder="请选择">
+                  <el-option
+                    v-for="item in modelOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>
+                <span class="color">颜色：</span>
+                <el-select class="input-width" v-model="colorValue" placeholder="请选择">
+                  <el-option
+                    v-for="item in colorOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>行驶证号：
+                <el-input class="input-width" placeholder="车牌号" v-model="input2"></el-input>
+                <el-button size="small" type="success">照片上传</el-button>
+              </div>
+              <div class="itemDet">
+                车辆座位：
+                <el-select class="input-width" v-model="carSeat" placeholder="请选择">
+                  <el-option
+                    v-for="item in seatOptions"
+                    :key="item.value"
+                    :label="item.value"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>车辆载重：
+                <el-select class="input-width" v-model="value1" placeholder="请选择">
+                  <el-option v-for="item in seatOptions" :key="item.value" :value="item.value"></el-option>
+                </el-select>车架号：
+                <el-input class="input-width" placeholder="车牌号" v-model="input2"></el-input>发动机号：
+                <el-input class="input-width" placeholder="车牌号" v-model="input2"></el-input>
+              </div>
+            </div>
+            <div class="two">
+              <p>内部车辆附加：</p>
+              <div class="itemDet">
+                所属单位：
+                <el-select class="input-width" v-model="carOfDept" placeholder="请选择">
+                  <el-option
+                    v-for="item in departmentOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>车辆类别1：
+                <el-select class="input-width" v-model="carType1" placeholder="请选择">
+                  <el-option
+                    v-for="item in propertyOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>
+                <span class="carstwo">类别2：</span>
+                <el-select class="input-width" v-model="carType2" placeholder="请选择">
+                  <el-option
+                    v-for="item in propertyOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>
+              </div>
+              <div class="itemDet">
+                车辆权限：
+                <el-select class="input-width" v-model="carRight" placeholder="请选择">
+                  <el-option
+                    v-for="item in rightOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>
+                <span class="datas">登记日：</span>
+                <el-date-picker class="input-width" v-model="value1" type="date" placeholder="选择日期"></el-date-picker>授权有效期：
+                <el-date-picker class="input-width" v-model="value2" type="date" placeholder="选择日期"></el-date-picker>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div class="footer">
-        <div class="one">
-          <p>车辆基本信息：</p>
-          <div class="itemDet">
-            车辆品牌：
-            <el-select class="input-width" v-model="brandValue" placeholder="请选择">
-              <el-option
-                v-for="item in brandOptions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select>车辆型号：
-            <el-select class="input-width" v-model="modelValue" placeholder="请选择">
-              <el-option
-                v-for="item in modelOptions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select><span class="color">颜色：</span>
-            <el-select class="input-width" v-model="colorValue" placeholder="请选择">
-              <el-option
-                v-for="item in colorOptions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select>行驶证号：
-            <el-input class="input-width" placeholder="车牌号" v-model="input2"></el-input>
-            <el-button size="small" type="success" >照片上传</el-button>
+        </el-tab-pane>
+        <el-tab-pane label="外部车辆/临时车辆" name="second">
+          <div class="header-container">
+            <div class="carImg">车辆照片</div>
+            <div class="carDet">
+              <div class="license">
+                奔驰2号
+                <span class="used">在用</span>
+              </div>
+              <div>
+                车辆类型
+                <el-select class="input-width" v-model="inoutValue" placeholder="请选择">
+                  <el-option
+                    v-for="item in inoutOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>
+              </div>
+            </div>
           </div>
-          <div class="itemDet">
-            车辆座位：
-            <el-select class="input-width" v-model="carSeat" placeholder="请选择">
-              <el-option
-                v-for="item in seatOptions"
-                :key="item.value"
-                :label="item.value"
-                :value="item.value"
-              ></el-option>
-            </el-select>车辆载重：
-            <el-select class="input-width" v-model="value1" placeholder="请选择">
-              <el-option
-                v-for="item in seatOptions"
-                :key="item.value"
-                :value="item.value"
-              ></el-option>
-            </el-select>车架号：
-            <el-input class="input-width" placeholder="车牌号" v-model="input2"></el-input>发动机号：
-            <el-input class="input-width" placeholder="车牌号" v-model="input2"></el-input>
+          <div class="one">
+            <p>外部车辆附加：</p>
+            <div class="itemDet">
+              车主姓名：
+              <el-input class="input-width" placeholder="请输入" v-model="hah1"></el-input>电话：
+              <el-input class="input-width" placeholder="请输入" v-model="hahhsd"></el-input>部队联系人：
+              <el-input class="input-width" placeholder="请输入" v-model="jhh"></el-input>联系人部门：
+              <el-select class="input-width" v-model="bumen" placeholder="请输入">
+                <el-option
+                  v-for="item in departmentOptions"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </div>
+            <div class="itemDet">
+              车辆权限：
+              <el-select class="input-width" v-model="carRight" placeholder="请选择">
+                <el-option
+                  v-for="item in rightOptions"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>权限有效期
+              <el-date-picker class="input-width" v-model="value2" type="date" placeholder="选择日期"></el-date-picker>
+            </div>
+            <div class="itemDet">
+              身份证：
+              <el-input class="input-width" placeholder="请输入" v-model="input2"></el-input>
+              <el-button size="small" type="success">身份证正面上传</el-button>
+              <el-button size="small" type="success">身份证背面上传</el-button>
+            </div>
+            <div class="itemDet">
+              驾驶证：
+              <el-input class="input-width" placeholder="请输入" v-model="input2"></el-input>
+              <el-button size="small" type="success">驾驶证正面上传</el-button>
+              <el-button size="small" type="success">驾驶证背面上传</el-button>
+            </div>
           </div>
-        </div>
-        <div class="two">
-          <p>内部车辆附加：</p>
-          <div class="itemDet">
-            所属单位：
-            <el-select class="input-width" v-model="carOfDept" placeholder="请选择">
-              <el-option
-                v-for="item in departmentOptions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select>车辆类别1：
-            <el-select class="input-width" v-model="carType1" placeholder="请选择">
-              <el-option
-                v-for="item in propertyOptions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select><span class="carstwo">类别2：</span>
-            <el-select class="input-width" v-model="carType2" placeholder="请选择">
-              <el-option
-                v-for="item in propertyOptions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select>
-          </div>
-          <div class="itemDet" >
-            车辆权限：
-            <el-select class="input-width" v-model="carRight" placeholder="请选择">
-              <el-option
-                v-for="item in rightOptions"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select><span class="datas">登记日：</span>
-            <el-date-picker class="input-width" v-model="value1" type="date" placeholder="选择日期"></el-date-picker>授权有效期：
-            <el-date-picker class="input-width" v-model="value2" type="date" placeholder="选择日期"></el-date-picker>
-          </div>
-        </div>
-      </div>
-      <div class="one">
-        <p>外部车辆附加：</p>
-        <div class="itemDet">
-          车主姓名：
-          <el-input class="input-width" placeholder="请输入" v-model="hah1"></el-input>电话：
-          <el-input class="input-width" placeholder="请输入" v-model="hahhsd"></el-input>部队联系人：
-          <el-input class="input-width" placeholder="请输入" v-model="jhh"></el-input>联系人部门：
-          <el-select class="input-width" v-model="bumen" placeholder="请输入">
-            <el-option
-              v-for="item in departmentOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-        </div>
-        <div class="itemDet">
-          车辆权限：
-          <el-select class="input-width" v-model="carRight" placeholder="请选择">
-            <el-option
-              v-for="item in rightOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select>权限有效期
-          <el-date-picker class="input-width" v-model="value2" type="date" placeholder="选择日期"></el-date-picker>
-        </div>
-        <div class="itemDet">
-          身份证：
-          <el-input class="input-width" placeholder="请输入" v-model="input2"></el-input>
-          <el-button size="small" type="success" >身份证正面上传</el-button>
-          <el-button size="small" type="success" >身份证背面上传</el-button>
-        </div>
-        <div class="itemDet">
-          驾驶证：
-          <el-input class="input-width" placeholder="请输入" v-model="input2"></el-input>
-          <el-button size="small" type="success" >驾驶证正面上传</el-button>
-          <el-button size="small" type="success" >驾驶证背面上传</el-button>
-        </div>
-      </div>
-      <div class="flex-center itemDet" style="margin-top:20px;"> 
-        <el-button class="editor-btn" type="danger" @click="handleBack" >取消</el-button>
-        <el-button class="editor-btn" type="success" style="margin-left:40px;">确定</el-button> 
+        </el-tab-pane>
+      </el-tabs>
+      <div class="flex-center itemDet" style="margin-top:20px;">
+        <el-button class="editor-btn" type="danger" @click="handleBack">取消</el-button>
+        <el-button class="editor-btn" type="success" style="margin-left:40px;">确定</el-button>
       </div>
     </div>
   </div>
@@ -256,14 +281,14 @@ export default {
           label: "临时车辆"
         }
       ],
-      propertyValue: "",//车辆属性1-内部 2-外部 3-临时
-      departmentValue: "",//单位
-      carOfDept:"",//车辆单位
-      carType1:"",//type1
-      carType2:"",//
-      inoutValue: "",//出入库状态
-      brandValue:"",//品牌
-      brandOptions:[
+      propertyValue: "", //车辆属性1-内部 2-外部 3-临时
+      departmentValue: "", //单位
+      carOfDept: "", //车辆单位
+      carType1: "", //type1
+      carType2: "", //
+      inoutValue: "", //出入库状态
+      brandValue: "", //品牌
+      brandOptions: [
         {
           value: "1",
           label: "丰田"
@@ -277,8 +302,8 @@ export default {
           label: "本田"
         }
       ],
-      modelValue:"",//型号
-      modelOptions:[
+      modelValue: "", //型号
+      modelOptions: [
         {
           value: "1",
           label: "途观"
@@ -292,8 +317,8 @@ export default {
           label: "捷达"
         }
       ],
-      colorValue:"",//颜色
-      colorOptions:[
+      colorValue: "", //颜色
+      colorOptions: [
         {
           value: "1",
           label: "红色"
@@ -307,36 +332,36 @@ export default {
           label: "绿色"
         }
       ],
-      VehicleNo:"",//行驶证号
-      driverNo:"",//驾驶证号
-      carSeat:5,//座位数
-      seatOptions:[
+      VehicleNo: "", //行驶证号
+      driverNo: "", //驾驶证号
+      carSeat: 5, //座位数
+      seatOptions: [
         {
-          label:"1",
+          label: "1",
           value: "1"
         },
         {
-          label:"2",
+          label: "2",
           value: "2"
         },
         {
-          label:"3",
+          label: "3",
           value: "3"
         }
       ],
-      carLoad:"",//车辆载重
-      frameNo:"",//车架号
-      engineNo:"",//发动机号
-      carType1:"",//
-      carType2:"",//
-      carRight:"",//
-      date1:"",
-      date2:"",
-      bumen:"",
-      hah1:"",
-      jhh:"",
-      hahhsd:"",
-      rightOptions:[
+      carLoad: "", //车辆载重
+      frameNo: "", //车架号
+      engineNo: "", //发动机号
+      carType1: "", //
+      carType2: "", //
+      carRight: "", //
+      date1: "",
+      date2: "",
+      bumen: "",
+      hah1: "",
+      jhh: "",
+      hahhsd: "",
+      rightOptions: [
         {
           value: "1",
           label: "最高权限"
@@ -396,7 +421,7 @@ export default {
     handleDelete(index, row) {
       console.log(index, row);
     },
-      handleBack(){
+    handleBack() {
       history.go(-1);
     }
   }
@@ -405,7 +430,7 @@ export default {
 <style scoped>
 .input-width {
   /* width: 100px; */
-  width:150px;
+  width: 150px;
   margin: 0 10px;
 }
 .header-container {
@@ -416,7 +441,7 @@ export default {
   width: 100px;
   height: 120px;
   vertical-align: middle;
-  border: 1px solid ;
+  border: 1px solid;
   text-align: center;
   line-height: 120px;
 }
@@ -434,27 +459,28 @@ export default {
 .upload {
   display: inline-block;
 }
-.one,.two{
-    padding: 10px 20px;
-    border: 1px solid #d0d0d0;
-    margin-top: 5px;
+.one,
+.two {
+  padding: 10px 20px;
+  border: 1px solid #d0d0d0;
+  margin-top: 5px;
 }
 .itemDet[data-v-b889b1ce] {
-    padding: 7px 0 7px 80px;
+  padding: 7px 0 7px 80px;
 }
-.color{
-    display: inline-block;
-    width: 68px;
+.color {
+  display: inline-block;
+  width: 68px;
 }
-.datas{
+.datas {
   display: inline-block;
   width: 93px;
 }
-.carstwo{
+.carstwo {
   display: inline-block;
   width: 100px;
 }
-.used{
+.used {
   display: inline-block;
   width: 40px;
   height: 20px;
