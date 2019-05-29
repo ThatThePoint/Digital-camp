95fd4b30928cb8a5a64ae6b409a27c7f635a5f48
battleingame <you@example.com>
2019-05-29 23:45:08 +0800

    添加人员修改

---
+++ src/components/page/DataCenter/registrationSheet/jiashu.vue
@@ -0,0 +1,110 @@
+<template>
+  <el-form ref="form" :model="form" label-width="80px">
+    <el-row>
+      <el-col :span="6">
+        <el-form-item label="姓名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="6">
+        <el-form-item label="性别">
+          <el-radio-group v-model="form.gender">
+            <el-radio label="男"></el-radio>
+            <el-radio label="女"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-col>
+      <el-col :span="6">
+        <el-form-item label="出生日期">
+          <el-date-picker class="input-width" v-model="birthday" type="date" placeholder="选择日期"></el-date-picker>
+        </el-form-item>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6">
+        <el-form-item label="籍贯">
+          <el-input v-model="form.origin"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="6">
+        <el-form-item label="民族">
+          <el-input v-model="form.national"></el-input>
+        </el-form-item>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6">
+        <el-form-item label="出生地">
+          <el-input v-model="form.bornArea"></el-input>
+        </el-form-item>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6">
+        <el-form-item label="工作单位">
+          <el-input v-model="form.workStation"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="6">
+        <el-form-item label="联系电话">
+          <el-input v-model="form.tel"></el-input>
+        </el-form-item>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="10">
+        <el-form-item label="家庭住址">
+          <el-input v-model="form.address"></el-input>
+        </el-form-item>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6">
+        <el-form-item label="身份证">
+          <el-input v-model="form.idcard"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="6">
+        <el-form-item label="婚姻状况">
+          <el-select v-model="form.marry" placeholder="请选择">
+            <el-option label="未婚" value="shaasanghai"></el-option>
+            <el-option label="已婚" value="beisdjing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+    </el-row>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        //以下才是对的数据
+        tid: "",
+        birthday:"",
+        name: "",
+        gender: "",
+        birthday: "",
+        origin: "",
+        national: "",
+        bornArea: "",
+        workStation: "",
+        tel: "",
+        address: "",
+        idcard: "",
+        marry: "",
+      }
+    };
+  },
+  methods: {
+    onSubmit() {},
+       handleClick(){},
+
+  }
+};
+</script>
