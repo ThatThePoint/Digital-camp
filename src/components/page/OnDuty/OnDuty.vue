305402be2f1590832f8cc0037a71f224a8258bf7
e <578356757@qq.com>
2019-06-02 22:58:17 +0800

    值班执勤

--- src/components/page/OnDuty/OnDuty.vue
+++ src/components/page/OnDuty/OnDuty.vue
@@ -12,18 +12,11 @@
             <el-table
               :data="tableData"
               style="width: 100%"
-              :default-sort="{prop: 'job', order: 'descending'}"
+              :default-sort="{prop: 'posiName', order: 'descending'}"
             >
-              <el-table-column prop="job" label="岗位名称" sortable width="180">{{}}</el-table-column>
+              <el-table-column prop="posiName" label="岗位名称" sortable width="180">{{}}</el-table-column>
               <el-table-column prop="dept" label="单位" sortable width="180"></el-table-column>
-              <el-table-column prop="dutyNow" label="当前值班人"></el-table-column>
-              <el-table-column prop="dutyToday" label="当日值班安排"></el-table-column>
-              <el-table-column prop="remark" label="值班记录"></el-table-column>
-              <el-table-column label="操作">
-                <template slot-scope="scope">
-                  <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                </template>
-              </el-table-column>
+              <el-table-column prop="curDuty" label="当前值班人"></el-table-column>
             </el-table>
           </div>
         </el-tab-pane>
@@ -34,25 +27,26 @@
                   <el-select
                     label="部门"
                     class="input-width"
-                    v-model="value"
+                    v-model="gangwei"
                     filterable
                     placeholder="所属部门"
+                    @change="selected"
                   >
                     <el-option
                       v-for="item in depts"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value"
+                      :key="item.tid"
+                      :label="item.name"
+                      :value="item.tid"
                     ></el-option>
                   </el-select>
                 </el-form-item>
                 <el-form-item label="岗位" :label-width="formLabelWidth">
-                  <el-select class="input-width" v-model="value" filterable placeholder="岗位">
+                  <el-select class="input-width" v-model="gangweidetail" filterable placeholder="岗位">
                     <el-option
                       v-for="item in jobs"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value"
+                      :key="item.tid"
+                      :label="item.name"
+                      :value="item.tid"
                     ></el-option>
                   </el-select>
                 </el-form-item>
@@ -72,12 +66,12 @@
                     </el-date-picker>
               </el-form-item>
               <el-form-item label="值班人" :label-width="formLabelWidth">
-                <el-select class="input-width" v-model="value" filterable placeholder="值班人">
+                <el-select class="input-width" v-model="persons" filterable placeholder="值班人">
                   <el-option
                     v-for="item in person"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value"
+                    :key="item.tid"
+                    :label="item.name"
+                    :value="item.tid"
                   ></el-option>
                 </el-select>
               </el-form-item>
@@ -104,7 +98,7 @@
           </div>
           <div class="body">
             <el-table
-              :data="tableData"
+              :data="searchtableData"
               style="width: 100%"
               :default-sort="{prop: 'job', order: 'descending'}"
             >
@@ -130,6 +124,12 @@ export default {
   name: "documentManagement",
   data() {
     return {
+      persons: "",//值班人
+      gangweidetail: '',//岗位值
+      gangwei: '',//岗位部门值
+      jobDatalist: [],//保存岗位的临时数组
+      personlist: [],//保存人员的临时数组
+      searchtableData: [],//值班查询
       value3:"",
       value4:"",
       pickerOptions: {
@@ -165,90 +165,11 @@ export default {
       value1: "",
       value2: "",
       input2: "",
-      depts: [
-        {
-          value: "1",
-          label: "信息科"
-        },
-        {
-          value: "2",
-          label: "保卫处"
-        },
-        {
-          value: "3",
-          label: "后勤处"
-        },
-        {
-          value: "4",
-          label: "警告"
-        }
-      ],
-      jobs: [
-        {
-          value: "1",
-          label: "班长"
-        },
-        {
-          value: "2",
-          label: "后勤员"
-        },
-        {
-          value: "3",
-          label: "科长"
-        }
-      ],
-      person: [
-        {
-          value: "1",
-          label: "张大锤"
-        },
-        {
-          value: "2",
-          label: "刘力"
-        },
-        {
-          value: "3",
-          label: "哈哈"
-        }
-      ],
+      depts: [],//岗位部门
+      jobs: [],//岗位
+      person: [],//r人员列表
       value: "",
-      tableData: [
-        {
-          job: "门卫",
-          dept: "保卫处",
-          dutyNow: "王小虎",
-          dutyToday: "8:00-20:00 王小虎<br>20:00-24:00 王小虎",
-          remark: "值班正常"
-        },
-        {
-          job: "门卫",
-          dept: "保卫处",
-          dutyNow: "王小虎",
-          dutyToday: "8:00-20:00王小虎</br>20:00-24:00 王小虎",
-          remark: "值班正常"
-        },
-        {
-          job: "门卫",
-          dept: "保卫处",
-          dutyNow: "王小虎",
-          dutyToday: "8:00-20:00王小虎</br>20:00-24:00 王小虎",
-          remark: "值班正常"
-        },
-        {
-          job: "门卫",
-          dept: "保卫处",
-          dutyNow: "王小虎",
-          dutyToday: "8:00-20:00王小虎</br>20:00-24:00 王小虎",
-          remark: "值班正常"
-        },
-        {
-          job: "门卫",
-          dept: "保卫处",
-          dutyNow: "王小虎",
-          dutyToday: "8:00-20:00王小虎</br>20:00-24:00 王小虎",
-          remark: "值班正常"
-        }
-      ],
+      tableData: [],
       dialogTableVisible: false,
       dialogFormVisible: false,
       form: {
@@ -264,19 +185,104 @@ export default {
       formLabelWidth: "120px"
     };
   },
+  created(){
+    let data = {
+      pageNum : 1,
+      pageSize : 25
+    }
+    let _this = this;
+    this.postAxios("/DailyOffice/JobRota",data)
+      .then(res => {
+        _this.tableData = res.list
+      })
+      .catch(err => {
+        console.log(err);
+    });
+  },
   methods: {
+    //选择岗位部门切换岗位
+    selected(){
+      this.jobs = []
+      this.person = []
+      this.gangweidetail = ''
+      this.persons = ''
+      for( let i = 0;i < this.jobDatalist.length; i++){
+        if(this.gangwei == this.jobDatalist[i].parentId){
+          this.jobs.push(this.jobDatalist[i])
+        }
+      }
+      for( let i = 0;i < this.personlist.length; i++){
+        if(this.gangwei == this.personlist[i].parentId){
+          this.person.push(this.personlist[i])
+        }
+      }
+    },   
     formatter(row, column) {
       return row.address;
     },
     handleEdit(index, row) {
-      console.log(index, row);
+
     },
     handleDelete(index, row) {
-      console.log(index, row);
+
     },
     handleClick(tab, event) {
-      console.log(tab, event);
-    }
+      this.jobDatalist = []
+      this.personlist = []
+      this.searchtableData = []
+      this.tableData = []
+      this.depts = []
+      this.jobs = []
+      this.person = []
+      let index = tab.index
+      if(index == 0){
+        let data = {
+          pageNum : 1,
+          pageSize : 25
+        }
+        let _this = this;
+        this.postAxios("/DailyOffice/JobRota",data)
+          .then(res => {
+            _this.tableData = res.list
+          })
+          .catch(err => {
+            console.log(err);
+        });
+      }else if(index == 1){
+        let data = {
+          pageNum : 1,
+          pageSize : 25
+        }
+        let _this = this;
+        this.postAxios("/DailyOffice/RotaInfo",data)
+          .then(res => {
+            _this.jobDatalist = res.jobData;
+            _this.personlist = res.dutyStaff
+            for(let i =0;i < res.jobData.length;i++){
+              if(res.jobData[i].parentId == '' || res.jobData[i].parentId == null){
+                _this.depts.push(res.jobData[i])
+              }
+            }
+          })
+          .catch(err => {
+            console.log(err);
+        });
+      }else if(index == 2){
+        let data = {
+          pageNum : 1,
+          pageSize : 25
+        }
+        let _this = this;
+        this.postAxios("/DailyOffice/RotaList",data)
+          .then(res => {
+            _this.searchtableData = res.list
+          })
+          .catch(err => {
+            console.log(err);
+        });
+
+      }
+    },
   }
 };
 </script>
