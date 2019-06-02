05ced7703aa033a6d91c2f3e6e017ae048cc8cfe
e <578356757@qq.com>
2019-06-02 17:19:48 +0800

    公文接口

--- src/components/page/DailyOffice/DocumentManagement.vue
+++ src/components/page/DailyOffice/DocumentManagement.vue
@@ -35,16 +35,16 @@
           </div>
           <div class="body">
             <el-table
-              :data="tableData"
+              :data="tableDataone"
               style="width: 100%"
               :default-sort="{prop: 'code', order: 'descending'}"
             >
-              <el-table-column prop="code" label="公文号" sortable width="180">{{}}</el-table-column>
-              <el-table-column prop="title" label="公文标题" sortable width="180"></el-table-column>
+              <el-table-column prop="code" label="公文号" sortable width="80">{{}}</el-table-column>
+              <el-table-column prop="title" label="公文标题" sortable width="120"></el-table-column>
               <el-table-column prop="messageLevel" label="公文等级" :formatter="formatterLever"></el-table-column>
               <el-table-column prop="sendDeptName" label="发送部门"></el-table-column>
               <el-table-column prop="docCreaterName" label="发送人"></el-table-column>
-              <el-table-column prop="docReceiversName" label="收件人"></el-table-column>
+              <el-table-column prop="docReceiversName" label="收件人" width="200"></el-table-column>
               <el-table-column prop="sendTime" label="发送时间"></el-table-column>
               <el-table-column prop="docStatus" label="状态" :formatter="formatterStaus"></el-table-column>
               <el-table-column prop="allReceive" label="全部已阅" :formatter="formatterReceive"></el-table-column>
@@ -81,7 +81,7 @@
           </div>
           <div class="body">
             <el-table
-              :data="tableData"
+              :data="tableDatatwo"
               style="width: 100%"
               :default-sort="{prop: 'date', order: 'descending'}"
             >
@@ -127,7 +127,7 @@
           </div>
           <div class="body">
             <el-table
-              :data="tableData"
+              :data="tableDatathree"
               style="width: 100%"
               :default-sort="{prop: 'date', order: 'descending'}"
             >
@@ -157,6 +157,9 @@ export default {
   name: "documentManagement",
   data() {
     return {
+      tableDataone: [],//未读公文列表
+      tableDatatwo: [],//已读公文列表
+      tableDatathree: [],//已发送公文列表
       activeName: 'first',
       level: "",
       title: "",
@@ -180,8 +183,64 @@ export default {
   },
   created(){
     this.getdata();
+    this.getTableDataone()
+    this.getTableDatatwo()
+    this.getTableDatathree()
   },
   methods: {
+    //未读
+    getTableDataone(){
+      let data = {
+        tab : "first",
+        title : this.title,//标题
+        level : this.level,//等级
+        start	: this.start,
+        end : this.end
+      }
+      this.postAxios("/DailyOffice/DocumentList",data)
+      .then(res => {
+        this.tableDataone = res.data
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+    });
+    },
+    //已读
+    getTableDatatwo(){
+      let data = {
+        tab : "secend",
+        title : this.title,//标题
+        level : this.level,//等级
+        start	: this.start,
+        end : this.end
+      }
+      this.postAxios("/DailyOffice/DocumentList",data)
+      .then(res => {
+        this.tableDatatwo = res.data
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+    });
+    },
+    //已发送
+    getTableDatathree(){
+      let data = {
+        tab : "third",
+        title : this.title,//标题
+        level : this.level,//等级
+        start	: this.start,
+        end : this.end
+      }
+      this.postAxios("/DailyOffice/DocumentList",data)
+      .then(res => {
+        this.tableDatathree = res.data
+      })
+      .catch(err => {
+        console.log(err);
+    });
+    },
     formatterStaus(row, column) {
       return row.status == 1 ? "发布" : "草稿";
     },
