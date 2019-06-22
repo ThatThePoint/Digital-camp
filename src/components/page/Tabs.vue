d3d254995fd146fd6331c53d2a39c3404d2f6758
e <578356757@qq.com>
2019-06-22 21:45:48 +0800

    form表单

--- src/components/page/Tabs.vue
+++ src/components/page/Tabs.vue
@@ -8,62 +8,10 @@
         <div class="container">
             <el-tabs v-model="message">
                 <el-tab-pane :label="`未读消息(${unread.length})`" name="first">
-                    <el-table :data="unread" :show-header="false" style="width: 100%">
-                        <el-table-column>
-                            <template slot-scope="scope">
-                                <span class="message-title">{{scope.row.title}}</span>
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="date" width="180"></el-table-column>
-                        <el-table-column width="120">
-                            <template slot-scope="scope">
-                                <el-button size="small" @click="handleRead(scope.$index)">标为已读</el-button>
-                            </template>
-                        </el-table-column>
+                    <el-table :data="unread" :show-header="false" style="width: 100%" @row-click="details">
+                        <el-table-column prop="content"></el-table-column>
+                        <el-table-column prop="receiveTime"></el-table-column>
                     </el-table>
-                    <div class="handle-row">
-                        <el-button type="primary">全部标为已读</el-button>
-                    </div>
-                </el-tab-pane>
-                <el-tab-pane :label="`已读消息(${read.length})`" name="second">
-                    <template v-if="message === 'second'">
-                        <el-table :data="read" :show-header="false" style="width: 100%">
-                            <el-table-column>
-                                <template slot-scope="scope">
-                                    <span class="message-title">{{scope.row.title}}</span>
-                                </template>
-                            </el-table-column>
-                            <el-table-column prop="date" width="150"></el-table-column>
-                            <el-table-column width="120">
-                                <template slot-scope="scope">
-                                    <el-button type="danger" @click="handleDel(scope.$index)">删除</el-button>
-                                </template>
-                            </el-table-column>
-                        </el-table>
-                        <div class="handle-row">
-                            <el-button type="danger">删除全部</el-button>
-                        </div>
-                    </template>
-                </el-tab-pane>
-                <el-tab-pane :label="`回收站(${recycle.length})`" name="third">
-                    <template v-if="message === 'third'">
-                        <el-table :data="recycle" :show-header="false" style="width: 100%">
-                            <el-table-column>
-                                <template slot-scope="scope">
-                                    <span class="message-title">{{scope.row.title}}</span>
-                                </template>
-                            </el-table-column>
-                            <el-table-column prop="date" width="150"></el-table-column>
-                            <el-table-column width="120">
-                                <template slot-scope="scope">
-                                    <el-button @click="handleRestore(scope.$index)">还原</el-button>
-                                </template>
-                            </el-table-column>
-                        </el-table>
-                        <div class="handle-row">
-                            <el-button type="danger">清空回收站</el-button>
-                        </div>
-                    </template>
                 </el-tab-pane>
             </el-tabs>
         </div>
@@ -77,13 +25,7 @@
             return {
                 message: 'first',
                 showHeader: false,
-                unread: [{
-                    date: '2018-04-19 20:00:00',
-                    title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护',
-                },{
-                    date: '2018-04-19 21:00:00',
-                    title: '今晚12点整发大红包，先到先得',
-                }],
+                unread: [],
                 read: [{
                     date: '2018-04-19 20:00:00',
                     title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
@@ -94,7 +36,31 @@
                 }]
             }
         },
+        created(){
+            this.getData()
+        },
         methods: {
+            getData(){
+                let _this = this
+                this.postAxios("/home/GetMessage",{})
+                    .then(res => {
+                    console.log(res)
+                    // _this.tableData = res.careList;
+                    _this.unread = res.data
+                    console.log(res)
+                    })
+                    .catch(err => {
+                    console.log(err);
+                });
+            },
+            details(row){
+                this.$router.push({
+                    path : row.path,
+                    query : {
+                        type : row.type
+                    }
+                })
+            },
             handleRead(index) {
                 const item = this.unread.splice(index, 1);
                 console.log(item);
